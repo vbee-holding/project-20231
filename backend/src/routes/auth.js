70920b05bf1cd75e9395b320ce2704553e897757
LeHung560 <hungx5qt2002@gmail.com>
2023-12-22 22:06:24 +0700

    update one tap google

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -21,7 +21,7 @@ router.get(
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    successRedirect: "/",
+    successRedirect: "http://localhost:3000/r/trending",
     failureRedirect: "/user/login",
   })
 );
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,20 +1,14 @@
 "use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import {GoogleLogin, useGoogleOneTapLogin } from '@react-oauth/google';
+import OneTap from "./oneTap";
+import { GoogleSignInButton, ButtonLogin } from "./authButton";
+import { getSession } from "@/utils/getSession";
 
 const Header = () => {
-  const [loginData, setLoginData] = useState(false)
-  const login = useGoogleOneTapLogin({
-    onSuccess: credentialResponse => {
-      console.log(credentialResponse);
-    },
-    onError: () => {
-      console.log('Login Failed');
-    },
-  });
   return (
-    <header className="bg-custom fixed top-0 z-10 w-full h-16">
+    <header className="bg-header fixed top-0 z-10 w-full h-16">
+      {/* {!storedSession && <OneTap />} */}
+      <OneTap />
       <div className=" relative h-full max-w-4xl mx-auto px-4 py-2 flex justify-between items-center">
         <div className="grow">
           <Image
@@ -27,13 +21,10 @@ const Header = () => {
           />
         </div>
         <div className="flex">
-          <div className="flex justify-between items-center">
-            <div className="mr-1">
-              <button className="bg-gray-950 hover:bg-gray-900 active:bg-gray-800 text-white font-bold py-2 px-4 rounded-full w-max">
-                Log in
-              </button>
-            </div>
-          </div>
+          {/* <GoogleSignInButton /> */}
+          <ButtonLogin />
+          {/* {storedSession && <GoogleSignInButton />} */}
+          {/* {!storedSession && <ButtonLogin />} */}
         </div>
       </div>
     </header>
