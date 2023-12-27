5b6acd8d6fa725c90df55d7a75dbfcc156f8a6dc
LeHung560 <hungx5qt2002@gmail.com>
2023-12-28 06:48:23 +0700

    change ui header login logout

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,13 +2,28 @@
 import Image from "next/image";
 import OneTap from "./oneTap";
 import { GoogleSignInButton, ButtonLogin } from "./authButton";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import { getSession } from "@/utils/getSession";
 
 const Header = () => {
+  const storedSession = getSession();
+
+  const { data: session, status } = useSession({
+    initialData: { session: storedSession },
+  });
+  console.log(session);
+  useEffect(() => {
+    if (status === "authenticated") {
+      localStorage.setItem("userSession", JSON.stringify(session));
+    } else if (status === "unauthenticated") {
+      localStorage.removeItem("userSession");
+    }
+  }, [session]);
+
   return (
     <header className="bg-header fixed top-0 z-10 w-full h-16">
-      {/* {!storedSession && <OneTap />} */}
-      <OneTap />
+      {status === "unauthenticated" && <OneTap />}
       <div className=" relative h-full max-w-4xl mx-auto px-4 py-2 flex justify-between items-center">
         <div className="grow">
           <Image
@@ -21,10 +36,10 @@ const Header = () => {
           />
         </div>
         <div className="flex">
-          {/* <GoogleSignInButton /> */}
-          <ButtonLogin />
-          {/* {storedSession && <GoogleSignInButton />} */}
-          {/* {!storedSession && <ButtonLogin />} */}
+          {status === "authenticated" && (
+            <GoogleSignInButton image={session.user.image} name={session.user.name} email={session.user.email}/>
+          )}
+          {status === "unauthenticated" && <ButtonLogin />}
         </div>
       </div>
     </header>
