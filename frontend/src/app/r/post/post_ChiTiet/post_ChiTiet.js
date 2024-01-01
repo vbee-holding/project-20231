6efa701a2044fd8cad4ee3d79d85963be7c9c1f5
20200011-anhct <chu152330@gmail.com>
2024-01-01 23:20:27 +0700

    Thêm BaiViet
    Xem TomTat vaChiTiet

---
+++ frontend/src/app/r/post/post_ChiTiet/post_ChiTiet.js
@@ -0,0 +1,68 @@
+import {
+    faComment,
+    faEye,
+  } from "@fortawesome/free-solid-svg-icons";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import Image from "next/image";
+  import ToggleButton from "../post_ChiTiet/togglebutton"
+
+  const Item = (props) => {
+    return (
+      <div
+        className="flex items-center 
+        bg-neutral-200
+        mr-4 py-1 px-2 
+        rounded-3xl cursor-pointer"
+      >
+        <FontAwesomeIcon
+          icon={props.icon}
+          style={{
+            color: "black",
+            width: 20,
+            height: 20,
+            marginRight: 4,
+            color: "#33363F",
+          }}
+        />
+        <p className="text-sm">{`${props.amout} ${props.text}`}</p>
+        {/* {props.amout && <p className="inline">{props.amout}</p>}
+        {props.text && <p className="inline">{`${props.text}`}</p>} */}
+      </div>
+    );
+  };
+  const Post_ChiTiet = (props) => {
+    return (
+      <div>
+        <div className="max-w-2xl p-4 mx-auto">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <Image
+                className="cursor-pointer"
+                src={props.linkImg}
+                alt="avatar"
+                width={60}
+                height={60}
+              />
+              <p className="font-bold mx-2 cursor-pointer hover:underline">
+                {props.name}
+              </p>
+              <p>{props.created}</p>
+            </div>
+            <ToggleButton />
+          </div>
+          <div>
+            <h1 className="font-bold text-3xl my-2">{props.title}</h1>
+          </div>
+          <div className="flex mt-2">
+            <Item icon={faComment} amout={props.comment} text="comments"/>
+            <Item icon={faEye} amout={props.view} text="views" />
+            <div className="ml-auto">
+              
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-400 h-0.5 max-w-xl mx-auto"></div>
+      </div>
+    );
+  };
+  export default Post_ChiTiet;
