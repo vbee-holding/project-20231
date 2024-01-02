ba3a4a6c43468ef71bcf8cb9520d26dd2e6cbe54
LeHung560 <hungx5qt2002@gmail.com>
2024-01-02 10:49:13 +0700

    update xem bai

--- frontend/src/app/r/post/post_ChiTiet/page.js
+++ frontend/src/app/r/post/post_ChiTiet/page.js
@@ -1,8 +1,4 @@
-import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Post_ChiTiet from '../post_ChiTiet/post_ChiTiet';
-import Link from 'next/link'
-import InfiniteScroll from "react-infinite-scroll-component";
+import Post_ChiTiet from "./post_ChiTiet";
 export default function Home() {
   const datas = [
     {
@@ -13,24 +9,22 @@ export default function Home() {
       title: "Report for F17 !",
       overView:
         "Thread chỉ nhận report, nghiêm cấm mọi hành vi spam post, thảo luận/ tranh cãi/ chửi bới Học hỏi, mmds",
-        comments: [
-          { id: 1, content: "Comment 1", user: "user1" },
-          { id: 2, content: "Comment 2", user: "user2" },
-          { id: 3, content: "Comment 3", user: "user3" },
-          { id: 4, content: "Comment 4", user: "user4" },
-          { id: 5, content: "Comment 5", user: "user5" },
-          { id: 6, content: "Comment 6", user: "user6" },
-          { id: 7, content: "Comment 7", user: "user7" },
-          { id: 8, content: "Comment 8", user: "user8" },
-          { id: 9, content: "Comment 9", user: "user9" },
-          { id: 10, content: "Comment 10", user: "user10" },
-          { id: 11, content: "Comment 11", user: "user11" },
-
-        ],
+      comments: [
+        { id: 1, content: "Comment 1", user: "user1" },
+        { id: 2, content: "Comment 2", user: "user2" },
+        { id: 3, content: "Comment 3", user: "user3" },
+        { id: 4, content: "Comment 4", user: "user4" },
+        { id: 5, content: "Comment 5", user: "user5" },
+        { id: 6, content: "Comment 6", user: "user6" },
+        { id: 7, content: "Comment 7", user: "user7" },
+        { id: 8, content: "Comment 8", user: "user8" },
+        { id: 9, content: "Comment 9", user: "user9" },
+        { id: 10, content: "Comment 10", user: "user10" },
+        { id: 11, content: "Comment 11", user: "user11" },
+      ],
       view: 50,
       like: 50,
     },
-
   ];
 
   const generateComments = () => {
@@ -53,22 +47,23 @@ export default function Home() {
               overView={item.overView}
               comments={item.comments}
               view={item.view}
-              like={item.like}  
+              like={item.like}
             />
-                     {/* Hiển thị danh sách comment */}
-              <div className="comment-scroll mt-4 mx-4">
+            {/* Hiển thị danh sách comment */}
+            <div className="comment-scroll mt-4 mx-4">
               {item.comments.map((comment) => (
-                <div key={comment.id} className="bg-gray-100 p-4 rounded-lg mb-2">
+                <div
+                  key={comment.id}
+                  className="bg-gray-100 p-4 rounded-lg mb-2"
+                >
                   <p className="text-gray-800 font-bold">{comment.user}</p>
                   <p className="text-gray-700">{comment.content}</p>
                 </div>
               ))}
             </div>
-   
-      
           </div>
         ))}
       </div>
     </main>
   );
-};
+}
--- frontend/src/app/r/post/post_ChiTiet/post_ChiTiet.js
+++ frontend/src/app/r/post/post_ChiTiet/post_ChiTiet.js
@@ -4,7 +4,7 @@ import {
   } from "@fortawesome/free-solid-svg-icons";
   import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
   import Image from "next/image";
-  import ToggleButton from "../post_ChiTiet/togglebutton"
+  import ToggleButton from "./togglebutton"
 
   const Item = (props) => {
     return (
