c121fcc30784ebd54a45df795c289a5efd544d76
Hoàng Anh <hoanganhlclqd@gmail.com>
2024-01-03 22:09:54 +0700

    add eff to button in equivalent page

--- frontend/src/components/buttonGroupMenu.js
+++ frontend/src/components/buttonGroupMenu.js
@@ -2,17 +2,35 @@ const ButtonGroupMenu = () => {
   return (
     <div className="col-span-7">
       <a href="/" className="py-2 px-0.5">
-        <button className="bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs">
+        <button
+          className={
+            window.location.pathname === "/"
+              ? "bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs"
+              : "bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs"
+          }
+        >
           Home
         </button>
       </a>
       <a href="/r/trending" className="py-2 px-0.5">
-        <button className="bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs">
+        <button
+          className={
+            window.location.pathname === "/r/trending"
+              ? "bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs"
+              : "bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs"
+          }
+        >
           Trending
         </button>
       </a>
       <a href="/r/trending-topics" className="py-2 px-0.5">
-        <button className="bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs">
+        <button
+          className={
+            window.location.pathname === "/r/trending-topics"
+              ? "bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs"
+              : "bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 px-2 rounded-full text-xs"
+          }
+        >
           Trending topics
         </button>
       </a>
--- frontend/src/components/ui/searchBar.js
+++ frontend/src/components/ui/searchBar.js
@@ -39,7 +39,6 @@ const StyledInputBase = styled(InputBase)(() => ({
   width: '100%',
   paddingLeft: '0px',
   paddingRight: '40px',
-  paddingTop: '3px',
   height: '100%',
 }));
 
