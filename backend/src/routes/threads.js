3d7f60cc814f976e5cf6086bcbde896b5bc5aa58
BuiThucNam-20200857 <nam.bt200857@sis.hust.edu.vn>
2024-01-03 08:39:55 +0700

    Add hot trend threads api

--- backend/src/routes/threads.js
+++ backend/src/routes/threads.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const threadController = require('../controllers/ThreadController');
-const replyController = require('../controllers/ReplyController');
+const threadController = require("../controllers/ThreadController");
+const replyController = require("../controllers/ReplyController");
+const hotTrendThreads = require("../controllers/HotTrendThreadsController");
 
-router.get('/search', threadController.searchThread);
-router.get('/:threadId/replies/search', replyController.searchReply);
-router.get('/:threadId/replies', replyController.showReply);
-router.get('/:threadId/summary', threadController.showSummarizedThread);
-router.get('/:threadId', threadController.showThread);
-router.get('/', threadController.showAll);
+router.get("/hot-trend-threads", hotTrendThreads);
+router.get("/search", threadController.searchThread);
+router.get("/:threadId/replies/search", replyController.searchReply);
+router.get("/:threadId/replies", replyController.showReply);
+router.get("/:threadId/summary", threadController.showSummarizedThread);
+router.get("/:threadId", threadController.showThread);
+router.get("/", threadController.showAll);
 module.exports = router;
