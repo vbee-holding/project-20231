2d9491996a7fd1a1dc4bcce7763f7ec5327dee13
Hoàng Anh <hoanganhlclqd@gmail.com>
2024-01-04 01:51:27 +0700

    update menu bar

--- frontend/src/components/menuBar.js
+++ frontend/src/components/menuBar.js
@@ -2,27 +2,25 @@
 import * as React from "react";
 import { Icons } from "./icons";
 import ButtonGroupMenu from "./buttonGroupMenu";
-import { TextField } from "@mui/material";
 import SearchBar from "./ui/searchBar";
-import { useRouter } from "next/navigation";
 
 const MenuBar = () => {
   const [search, setSearch] = React.useState({
-    isSearching: false,
+    isSearch: false,
     searchContent: "",
   });
 
   const handleSearch = () => {
     setSearch({
       ...search,
-      isSearching: !search.isSearching,
+      isSearch: !search.isSearch,
     });
   };
 
   const handleSearching = () => {
     setSearch({
       ...search,
-      isSearching: !search.isSearching,
+      isSearch: !search.isSearch,
     });
   };
 
@@ -30,6 +28,7 @@ const MenuBar = () => {
     setSearch({
       ...search,
       searchContent: e,
+      isSearch: true,
     });
   };
 
@@ -42,45 +41,31 @@ const MenuBar = () => {
 
   return (
     <div className="inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-      {search.isSearching ? (
-        <div className="grid grid-cols-11 items-center px-4">
+      <div className="grid grid-cols-11 items-center px-4 flex-none">
+        {search.isSearch ? (
           <button onClick={handleSearch}>
             <Icons.back className="mr-2 h-4 w-4" />
           </button>
-
-          <div className="col-span-9">
-            <SearchBar
-              value={search.searchContent}
-              className="w-full"
-              onChange={handleInput}
-              onSearch={handleSearching}
-              onCancelResearch={handleCancel}
-            />
-          </div>
-          <div className="flex justify-end items-center">
-            <button onClick={handleSearching}>
-              <Icons.search className="relative w-300 h-40 border-r-5" />
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="grid grid-cols-11 items-center px-4">
+        ) : (
           <ButtonGroupMenu />
-          <div className="col-span-3">
-            <TextField
-              variant="standard"
-              className="w-fit text-xs"
-              onClick={handleSearch}
-              placeholder={search.searchContent}
-            />
-          </div>
-          <div className="flex justify-end items-center">
-            <button onClick={handleSearch}>
-              <Icons.search className="relative w-300 h-40 border-r-5" />
-            </button>
-          </div>
+        )}
+
+        <div className={search.isSearch ? "col-span-9" : "col-span-3"}>
+          <SearchBar
+            value={search.searchContent}
+            className="w-full py-0"
+            onChange={handleInput}
+            onSearch={handleSearching}
+            onCancelResearch={handleCancel}
+          />
         </div>
-      )}
+
+        <div className="flex justify-end items-center">
+          <button onClick={handleSearching}>
+            <Icons.search className="relative w-300 h-40 border-r-5" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
