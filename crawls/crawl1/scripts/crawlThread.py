c621a8cf3986846e9f90a1544b138cfaea5259d1
LeHung560 <hungx5qt2002@gmail.com>
2024-01-11 23:58:44 +0700

    crawl1-2

--- crawls/crawl1/scripts/crawlThread.py
+++ crawls/crawl1/scripts/crawlThread.py
@@ -21,7 +21,7 @@ def crawl_thread():
         database = client["test"]
         collection = database["threads"]
 
-        for i in range(2, 10):
+        for i in range(2, 100):
             url = f"https://voz.vn/f/chuyen-tro-linh-tinh.17/page-{i}"
             print(url)
             headersList = {
@@ -87,7 +87,7 @@ def crawl_thread():
                                 if all_links[-1].text:
                                     last_page = int(all_links[-1].text)
 
-                        check = 56
+                        check = 2
 
                         result.append(
                             {
