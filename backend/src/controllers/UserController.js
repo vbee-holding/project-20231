765ba68c1adb5b9ec2c039818c578ec3123a1a47
BuiThucNam-20200857 <nam.bt200857@sis.hust.edu.vn>
2024-01-19 16:15:01 +0700

    Add api change accept notifi

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,11 +1,18 @@
 const User = require("../models/user");
 const logger = require("../utils/logger");
+
 const saveUser = async (req, res) => {
   const { email, image, name, isNotifi } = req.body;
 
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser) res.status(200).send("Người dùng đã tồn tại") && logger.info({status: 200, message: "Người dùng đã tồn tại", data: req.body}) ;
+    if (existingUser)
+      res.status(200).send("Người dùng đã tồn tại") &&
+        logger.info({
+          status: 200,
+          message: "Người dùng đã tồn tại",
+          data: req.body,
+        });
     else {
       const newUser = new User({
         email: email,
@@ -19,12 +26,52 @@ const saveUser = async (req, res) => {
       console.log(isNotifi);
 
       await newUser.save();
-      res.status(200).send("Người dùng đã được thêm vào collection users")
-      logger.info({status: 200, message: "Người dùng đã được thêm vào collection users", data: newUser,url: req.originalUrl, method: req.method, sessionID: req.sessionID, headers: req.headers}) ;
+      res.status(200).send("Người dùng đã được thêm vào collection users");
+      logger.info({
+        status: 200,
+        message: "Người dùng đã được thêm vào collection users",
+        data: newUser,
+        url: req.originalUrl,
+        method: req.method,
+        sessionID: req.sessionID,
+        headers: req.headers,
+      });
     }
   } catch (error) {
-    res.status(500).send("Lỗi xử lý yêu cầu") && logger.error({status: 500, message: "Lỗi xử lý yêu cầu", data : req.body, url: req.originalUrl, method: req.method, sessionID: req.sessionID, headers: req.headers});
+    res.status(500).send("Lỗi xử lý yêu cầu") &&
+      logger.error({
+        status: 500,
+        message: "Lỗi xử lý yêu cầu",
+        data: req.body,
+        url: req.originalUrl,
+        method: req.method,
+        sessionID: req.sessionID,
+        headers: req.headers,
+      });
   }
 };
 
-module.exports = saveUser;
+const acceptNotify = async (req, res) => {
+  const googleId = req.params.googleId;
+  const { isNotifi } = req.body;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { googleId: googleId },
+      { isNotifi: isNotifi }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "Người dùng không tồn tại" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Đã xảy ra lỗi trong quá trình xử lý yêu cầu" });
+  }
+};
+
+module.exports = { saveUser, acceptNotify };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,9 @@
 const threadRouter = require("./threads");
-const saveUser = require("../controllers/UserController");
+const { saveUser, acceptNotify } = require("../controllers/UserController");
 
 function route(app) {
   app.post("/user/profile", saveUser);
+  app.put("/user/notify/:googleId", acceptNotify);
   app.use("/threads", threadRouter);
 }
 
