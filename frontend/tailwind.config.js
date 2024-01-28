8a21cadc9a2c60032cec231e69777918dd9afdca
LeHung560 <hungx5qt2002@gmail.com>
2024-01-28 15:34:03 +0700

    update package.json

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {
@@ -20,7 +19,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
+  plugins: [],
 }
