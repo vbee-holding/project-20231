a10353ae3777f3f196064032d1563ac2a979eda3
LeHung560 <hungx5qt2002@gmail.com>
2024-01-29 22:52:36 +0700

    update trending page

--- frontend/src/app/r/trending/page.js
+++ frontend/src/app/r/trending/page.js
@@ -3,47 +3,41 @@ import MenuBar from "@/components/menuBar";
 import Post from "@/components/post";
 import { useEffect, useState } from "react";
 import axios from "@/utils/axios";
+import Loader from "@/components/loader";
 
 const Trending = () => {
   const [items, setItems] = useState([]);
   const [loadedAxios, SetLoaded] = useState(false)
   useEffect(() => {
     axios
-      .get("threads/hot-trend-threads")
+      .get("trending/threads")
       .then((response) => {
-        setItems(response.data.topThreads);
+        setItems(response.data.hotThreads);
+        SetLoaded(true)
       })
       .catch((error) => console.log(error));
-    SetLoaded(true)
   }, []);
-  // Ham lay ra 20 chu cai dau tien
-  const truncate = (str) => {
-    const words = str.split(" ");
-    if (words.length <= 20) {
-      return str;
-    } else {
-      const truncatedWords = words.slice(0, 20);
-      return `${truncatedWords.join(" ")}...`;
-    }
-  };
   return (
     <div>
       <MenuBar />
       {items.map((item, index) => (
         <Post
-          key={index}
-          linkImg={item.avatarUrl}
-          name={item.author}
-          created={item.createdTime}
-          title={item.title}
-          // overView={truncate(item.replys[0].content)}
-          comment={item.totalReplies}
-          view={item.views}
-          // like={item.like}
+        key={index}
+        linkImg={item.avatarUrl}
+        name={item.author}
+        created={item.createdTime}
+        title={item.title}
+        overView={item.content}
+        comment={item.totalReplies}
+        view={item.views}
+        threadId={item.threadId}
         />
       ))}
       {items.length === 0 && loadedAxios
       && <p className="text-center mt-5">Không có bài viết nào</p>
+      }
+      {items.length === 0 && !loadedAxios
+      && <Loader />
       }
     </div>
   );
