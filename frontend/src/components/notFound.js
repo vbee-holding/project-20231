cb10164b5aa15e47bbbb075cd8202e0dee4b0366
20200015-anhh <hoanganhlclqd@gmail.com>
2024-01-30 21:03:58 +0700

    add time filter

--- frontend/src/components/notFound.js
+++ frontend/src/components/notFound.js
@@ -1,12 +1,12 @@
 const NotFound = () => {
   return (
-      <div className="inset -0 h-96 max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+      <div className="max-w-2xl mx-auto flex flex-col items-center py-44 gap-20">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             We couldn't find the result
           </h1>
           <p className="text-sm max-w-xs mx-auto">
-            Please double check your keyword
+            Please double check your keyword or filter
           </p>
         </div>
       </div>
