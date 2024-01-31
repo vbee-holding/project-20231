582420a2e547c49c214f0763093cb37c6f8380d6
NMP <132655206+20215458-phuongnm@users.noreply.github.com>
2024-01-31 11:04:33 +0700

    Update page.js

--- frontend/src/app/r/trending-topics/page.js
+++ frontend/src/app/r/trending-topics/page.js
@@ -1,51 +1,39 @@
+"use client";
 import MenuBar from "@/components/menuBar";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import axios from "@/utils/axios";
 
-const Item = ({ title }) => {
+const Item = (props) => {
   return (
-    <div className="flex-column items-center pl-8 mb-1 mx-2 overflow-hidden float-left ">
+    <div className="flex-column items-center pl-8 mb-7 mx-2 overflow-hidden float-left ">
       <Link href="./trending-topics-detail">
         <div>
           <div className="w-[143px] h-[87px] bg-zinc-300 rounded-[5px] border border-black color-inherit decoration-none outline-none;" />
           <h2 className="text-xl text-gray-800 mt-1 font-bold cursor-pointer text-center color-inherit decoration-none outline-none">
-            {title}
+            {props.topic}
           </h2>
         </div>
         <h2 className="text-0.5 mb-0.5 text-gray-800 cursor-pointer text-center">
-          10 bài viết{" "}
+          {`${props.amount} bài viết`}
         </h2>
       </Link>
     </div>
   );
 };
 
-const TrendingTopics = () => {
-  const datas = [
-    {
-      id: 1,
-      title: "Game",
-    },
-    {
-      id: 2,
-      title: "Space",
-    },
-    {
-      id: 3,
-      title: "Art",
-    },
-    {
-      id: 4,
-      title: "Weather",
-    },
-    {
-      id: 5,
-      title: "Black Friday",
-    },
-    {
-      id: 6,
-      title: "Sport",
-    },
-  ];
+const HotTrendTopics = () => {
+  const [items, setItems] = useState([]);
+  const [loadedAxios, SetLoaded] = useState(false)
+  useEffect(() => {
+    axios
+      .get("trending/topics")
+      .then((response) => {
+        setItems(response.data.trendingTopic);
+        SetLoaded(true)
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
       <MenuBar />
@@ -56,8 +44,12 @@ const TrendingTopics = () => {
         <div className="bg-gray-400 h-0.5 grow mt-12"></div>
       </div>
       <div>
-        {datas.map((child) => (
-          <Item key={child.id} title={child.title} />
+        {items.map((child, index) => (
+          <Item
+            key={index}
+            topic={child.tag}
+            amount={child.threadCount}
+          />
         ))}
       </div>
     </div>
